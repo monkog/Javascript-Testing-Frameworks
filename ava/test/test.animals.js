a26562cf94a14205733731c37f4013707c0e8114
monkog <monkog@wp.pl>
2018-09-12 17:48:27 +0200

    Better asserts in AVA

--- ava/test/test.animals.js
+++ ava/test/test.animals.js
@@ -1,22 +1,22 @@
 import test from 'ava';
 const Animals = require('../../src/animals');
 
-test.before(t=>{
+test.before(t => {
     console.log('run this method once before all tests');
 });
 
-test.beforeEach(t=>{
+test.beforeEach(t => {
     console.log('run this method before each test');
 });
 
 test('should be an async tortoise', async assert => {
-    await Animals.tortoise();
-    assert.pass();
+    let result = await Animals.tortoise();
+    assert.deepEqual(result, "I'm slow");
 });
 
 test('should be a promise tortoise', async assert => {
-    return Promise.resolve(Animals.tortoise()).then( result =>{
-        assert.pass();
+    return Promise.resolve(Animals.tortoise()).then(result => {
+        assert.deepEqual(result, "I'm slow");
     });
 });
 
@@ -31,10 +31,10 @@ test.after(t=>{
     console.log('run this method once after all tests');
 });
 
-test.after.always(t=>{
+test.after.always(t => {
     console.log('run this method once after all tests, even if there were some failures');
 });
 
-test.afterEach(t=>{
+test.afterEach(t => {
     console.log('run this method after each test');
 });
