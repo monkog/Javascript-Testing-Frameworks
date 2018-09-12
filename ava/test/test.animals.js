d146b3c42e1eabe2db4daa6f526e65de58c7eb70
monkog <monkog@wp.pl>
2018-09-12 12:49:04 +0200

    More AVA functionality
    Planning
    power asserts
    before/after(each)

--- ava/test/test.animals.js
+++ ava/test/test.animals.js
@@ -1,7 +1,40 @@
 import test from 'ava';
 const Animals = require('../../src/animals');
 
+test.before(t=>{
+    console.log('run this method once before all tests');
+});
+
+test.beforeEach(t=>{
+    console.log('run this method before each test');
+});
+
 test('should be an async tortoise', async assert => {
     await Animals.tortoise();
     assert.pass();
+});
+
+test('should be a promise tortoise', async assert => {
+    return Promise.resolve(Animals.tortoise()).then( result =>{
+        assert.pass();
+    });
+});
+
+test('power assert functionality', t => {
+	const a = /foo/;
+	const b = 'bar';
+	const c = 'baz';
+	t.true(a.test(b) || b === c);
+});
+
+test.after(t=>{
+    console.log('run this method once after all tests');
+});
+
+test.after.always(t=>{
+    console.log('run this method once after all tests, even if there were some failures');
+});
+
+test.afterEach(t=>{
+    console.log('run this method after each test');
 });
--- ava/test/test.calculator.js
+++ ava/test/test.calculator.js
@@ -10,6 +10,13 @@ test.skip('this test should not be run', assert => {
     assert.fail();
 });
 
+test('this test should skip an assert and pass', assert => {
+    assert.skip.fail();
+    assert.pass();
+});
+
+test.todo('a test for multiplication shall be written someday');
+
 test('this test should fail', assert => {
     assert.fail();
 });
@@ -20,4 +27,28 @@ test('this test has no assert', assert => {
 test('should return 3 when adding 1 to 2', assert => {
     let result = Calculator.add(1, 2);
     assert.is(result, 3);
+});
+
+test('test planning functionality', assert => {
+    assert.plan(1);
+    let result = Calculator.add(1, 2);
+    assert.is(result, 3);
+});
+
+test('test plan will pass no matter for skipped assert', assert => {
+    assert.plan(2);
+    let result = Calculator.add(1, 2);
+    assert.skip.is(result, 15);
+    assert.is(result, 3);
+});
+
+test('this plan will fail due to too many assertions', assert => {
+    assert.plan(1);
+    let result = Calculator.add(1, 2);
+    assert.is(result, 3);
+    assert.is(result, 3);
+});
+
+test.failing('we expect this test to be failing so it can be marked as known failure', assert => {
+    assert.fail();
 });
