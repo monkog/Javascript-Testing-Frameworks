5ff62e792f220ad4609bc6e1cb7d4a6682cc8dea
monkog <monkog@wp.pl>
2018-09-12 18:02:10 +0200

    Remove duplicated AVA test

--- ava/test/test.calculator.js
+++ ava/test/test.calculator.js
@@ -24,11 +24,6 @@ test('this test should fail', assert => {
 test('this test has no assert', assert => {
 });
 
-test('should return 3 when adding 1 to 2', assert => {
-    let result = Calculator.add(1, 2);
-    assert.is(result, 3);
-});
-
 test('test planning functionality', assert => {
     assert.plan(1);
     let result = Calculator.add(1, 2);
