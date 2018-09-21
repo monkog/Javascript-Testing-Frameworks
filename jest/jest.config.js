d2d114ef10a74dae5caa8aba529501a92b2038c2
monkog <monkog@wp.pl>
2018-09-21 14:32:42 +0200

    Jest configuration

---
+++ jest/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "verbose": false,
+    "bail": false,
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "../src/*.js"
+      ],
+    "coverageDirectory": "./coverage",
+    "notify": true
+};
